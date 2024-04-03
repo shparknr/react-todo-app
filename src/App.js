ace633141cd3977d8db7d49767dda2c365033e42
shparknr <shparknr11@naver.com>
2024-04-03 13:13:01 +0900

    designs :  할 일 앱 만들기 UI 구성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import TodoTemplate from "./components/TodoTemplate";
-
+import { MdAdd } from "react-icons/md";
+import TodoInsert from "./components/TodoInsert";
+import TodoListItem from "./components/TodoListItem";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return  <TodoTemplate></TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  );
 }
 
 export default App;
